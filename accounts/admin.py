455e8ebdcd9f3aa66f2457fa668651f71507c7e7
Flynnt Knapp <bruce.stull@gmail.com>
2023-05-07 09:53:16 -0400

    Add 'get_database_config_variables' function.

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,6 +11,28 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = (
-        'username',
-        'email'
+        "username",
+        "email",
+        "registration_accepted",
+        "is_staff",
     )
+
+    def get_fieldsets(self, request, obj=None):
+        """
+        Override `get_fieldsets()` to add `registration_accepted` to a `Moderator Permissions` section of `CustomUser` change view.
+        """
+        # Get the default `fieldsets` from the superclass `UserAdmin`:
+        fieldsets = super().get_fieldsets(request, obj)
+
+        # Convert fieldsets to list:
+        fieldsets_as_list = list(fieldsets)
+
+        # Create list of single tuple for `registration_accepted`:
+        moderator_permissions_as_list = (
+            "Moderator Permissions",
+            {"fields": ("registration_accepted",)},
+        )
+
+        fieldsets_as_list.insert(2, moderator_permissions_as_list)
+        # Combine the two lists and return the result:
+        return fieldsets_as_list
--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
     """
     A CustomUser class is added so we can add functionality later. It's more convenient then not to add CustomUser at beginning of project before database migrations are started.
     """
-    # Add additional fields in here and remove `pass` line below.
-    pass
+    registration_accepted = models.BooleanField(
+        default=False,
+        help_text="Designates whether this user's registration has been accepted.",
+    )
 
     def __str__(self):
         """
