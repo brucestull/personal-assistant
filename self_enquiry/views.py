7a592e6e572f781ffaa90923d71f0c39b20a4944
Flynnt Knapp <bruce.stull@gmail.com>
2023-05-25 05:34:21 -0400

    Add 'Journal' create URL, view, and template.

--- self_enquiry/views.py
+++ self_enquiry/views.py
@@ -2,6 +2,8 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse
 
 from config.settings.common import THE_SITE_NAME
 from .models import Journal
@@ -28,3 +30,24 @@ class JournalListView(ListView):
         Override the default queryset to only return journals that belong to the current user.
         """
         return super().get_queryset().filter(author=self.request.user)
+
+
+class JournalCreateView(CreateView):
+    """
+    Create view for a new `self_enquiry.Journal`.
+    """
+
+    model = Journal
+    fields = ["title", "content"]
+    extra_context = {
+        "the_site_name": THE_SITE_NAME,
+        "page_title": JOURNAL_CREATE_PAGE_TITLE,
+    }
+    success_url = "/journals/list/"
+
+    def form_valid(self, form):
+        """
+        Override the default form_valid method to add the current user to the `author` field.
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
