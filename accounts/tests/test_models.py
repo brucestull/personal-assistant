91887263e956f88ee3b55ce53abff45a075bbaae
Flynnt Knapp <bruce.stull@gmail.com>
2023-05-26 02:07:18 -0400

    Add tests for 'accounts' models and views.

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 from accounts.models import CustomUser
 
-A_TEST_USERNAME = 'ACustomUser'
+A_TEST_USERNAME = "ACustomUser"
+
+CUSTOM_USER_REGISTRATION_ACCEPTED_HELP_TEXT = (
+    "Designates whether this user's registration has been accepted."
+)
+
 
 class CustomUserModelTest(TestCase):
     """
@@ -19,6 +24,36 @@ class CustomUserModelTest(TestCase):
         """
         user = CustomUser.objects.create(
             username=A_TEST_USERNAME,
+        )
+
+    def test_registration_accepted_field_default_false_for_created_user(self):
+        """
+        A newly created `CustomUser` should have `registration_accepted` `False`.
+        """
+        user = CustomUser.objects.get(id=1)
+        self.assertFalse(user.registration_accepted)
+
+    def test_registration_accepted_default_attribute_false(self):
+        """
+        `CustomUser` model `registration_accepted` field `default` should be `False`.
+        """
+        user = CustomUser.objects.get(id=1)
+        field_registration_accepted = user._meta.get_field(
+            "registration_accepted",
+        )
+        self.assertEqual(field_registration_accepted.default, False)
+
+    def test_registration_accepted_help_text(self):
+        """
+        `CustomUser` model `registration_accepted` field `help_text` should be
+        `Designates whether this user's registration has been accepted.`.
+        """
+        user = CustomUser.objects.get(id=1)
+        self.assertEqual(
+            user._meta.get_field(
+                "registration_accepted",
+            ).help_text,
+            CUSTOM_USER_REGISTRATION_ACCEPTED_HELP_TEXT,
         )
 
     def test_dunder_string_method(self):
--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -8,6 +8,10 @@ A_TEST_USERNAME = "ACustomUser"
 A_TEST_PASSWORD = "a_test_password"
 A_TEST_FIRST_NAME = "A"
 
+ANOTHER_TEST_USERNAME = "AnotherCustomUser"
+ANOTHER_TEST_PASSWORD = "another_test_password"
+ANOTHER_TEST_FIRST_NAME = "Another"
+
 THE_SITE_NAME = "Health Activities"
 
 SIGN_UP_VIEW_URL = "/accounts/signup/"
@@ -18,8 +22,12 @@ CUSTOM_LOGIN_VIEW_URL = "/accounts/login/"
 CUSTOM_LOGIN_VIEW_NAME = "login"
 
 USER_UPDATE_VIEW_URL = "/accounts/1/edit/"
-USER_UPDATE_VIEW_NAME = "edit-profile"
+USER_UPDATE_VIEW_NAME = "edit"
 USER_UPDATE_VIEW_TEMPLATE = "registration/update.html"
+
+USER_DETAIL_VIEW_URL = "/accounts/1/detail/"
+USER_DETAIL_VIEW_NAME = "detail"
+USER_DETAIL_VIEW_TEMPLATE = "accounts/customuser_detail.html"
 
 
 class SignUpViewTest(TestCase):
@@ -48,10 +56,16 @@ class SignUpViewTest(TestCase):
         response = self.client.get(SIGN_UP_VIEW_URL)
         self.assertEqual(response.status_code, 200)
         # Test that the form is an instance of `CustomUserCreationForm`.
-        self.assertIsInstance(response.context["form"], CustomUserCreationForm)
+        self.assertIsInstance(
+            response.context["form"],
+            CustomUserCreationForm,
+        )
         # Alternative ways to test the form:
         # Test that the form's class is `CustomUserCreationForm`.
-        self.assertEqual(response.context["form"].__class__, CustomUserCreationForm)
+        self.assertEqual(
+            response.context["form"].__class__,
+            CustomUserCreationForm,
+        )
         # Test that the form's class name is the string `CustomUserCreationForm`.
         self.assertEqual(
             response.context["form"].__class__.__name__, "CustomUserCreationForm"
@@ -83,11 +97,14 @@ class SignUpViewTest(TestCase):
     def test_context_has_the_site_name(self):
         """
 
-        View `context` should have a value for THE_SITE_NAME.
+        View `context` should have a value of "Health Activities" for THE_SITE_NAME.
         """
         response = self.client.get(SIGN_UP_VIEW_URL)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["the_site_name"], THE_SITE_NAME)
+        self.assertEqual(
+            response.context["the_site_name"],
+            THE_SITE_NAME,
+        )
 
 
 class CustomLoginViewTest(TestCase):
@@ -115,12 +132,15 @@ class CustomLoginViewTest(TestCase):
         """
         response = self.client.get(CUSTOM_LOGIN_VIEW_URL)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["the_site_name"], THE_SITE_NAME)
+        self.assertEqual(
+            response.context["the_site_name"],
+            THE_SITE_NAME,
+        )
 
 
-class UserUpdateViewTest(TestCase):
+class CustomUserUpdateViewTest(TestCase):
     """
-    Tests for `UserUpdateView`.
+    Tests for `CustomUserUpdateView`.
     """
 
     @classmethod
@@ -132,24 +152,157 @@ class UserUpdateViewTest(TestCase):
             username=A_TEST_USERNAME,
             password=A_TEST_PASSWORD,
         )
-        pass
 
-    # TODO: Why is this not working as expected?
-    def test_url_redirects_for_non_authenticated_user(self):
+    def test_url_redirects_non_authenticated_user(self):
         """
         URL USER_UPDATE_VIEW_URL should redirect for non-authenticated user.
         """
-        # response = self.client.get(
-        #     reverse(
-        #         USER_UPDATE_VIEW_NAME,
-        #         kwargs={"pk": self.a_test_user.pk},
-        #     )
-        # )
-        # self.assertRedirects(
-        #     response,
-        #     CUSTOM_LOGIN_VIEW_URL + "?next=" + USER_UPDATE_VIEW_URL,
-        #     status_code=302,
-        #     target_status_code=200,
-        #     fetch_redirect_response=True,
-        # )
-        pass
+        response = self.client.get(
+            reverse(
+                USER_UPDATE_VIEW_NAME,
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertRedirects(
+            response,
+            CUSTOM_LOGIN_VIEW_URL + "?next=" + USER_UPDATE_VIEW_URL,
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True,
+        )
+
+    # TODO: Test uses correct form.
+
+    def test_view_uses_correct_template(self):
+        """
+        View should use USER_UPDATE_VIEW_TEMPLATE.
+        """
+        self.client.force_login(self.a_test_user)
+        response = self.client.get(
+            reverse(
+                USER_UPDATE_VIEW_NAME,
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, USER_UPDATE_VIEW_TEMPLATE)
+
+    def test_current_user_is_context_object(self):
+        """
+        User should only be able to edit their own account.
+        """
+        self.client.force_login(self.a_test_user)
+        response = self.client.get(
+            reverse(
+                USER_UPDATE_VIEW_NAME,
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["object"], self.a_test_user)
+
+    def test_view_returns_403_for_user_updating_another_user(self):
+        """
+        User should not be able to edit another user's account.
+        """
+        another_test_user = CustomUser.objects.create_user(
+            username=ANOTHER_TEST_USERNAME,
+            password=ANOTHER_TEST_PASSWORD,
+        )
+        self.client.force_login(another_test_user)
+        response = self.client.get(
+            reverse(
+                USER_UPDATE_VIEW_NAME,
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 403)        
+
+    def test_the_site_name_in_context(self):
+        """
+        View `context` should have a value for THE_SITE_NAME.
+        """
+        self.client.force_login(self.a_test_user)
+        response = self.client.get(
+            reverse(
+                USER_UPDATE_VIEW_NAME,
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["the_site_name"], THE_SITE_NAME)
+
+
+class CustomUserDetailViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Create a test user and add it as an attribute of the `cls`.
+        """
+        cls.a_test_user = CustomUser.objects.create_user(
+            username=A_TEST_USERNAME,
+            password=A_TEST_PASSWORD,
+        )
+
+    def test_url_redirects_non_authenticated_user(self):
+        """
+        URL USER_UPDATE_VIEW_URL should redirect for non-authenticated user.
+        """
+        response = self.client.get(
+            reverse(
+                "detail",
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertRedirects(
+            response,
+            CUSTOM_LOGIN_VIEW_URL + "?next=" + USER_DETAIL_VIEW_URL,
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True,
+        )
+
+    def test_view_uses_correct_template(self):
+        """
+        View should use USER_DETAIL_VIEW_TEMPLATE.
+        """
+        self.client.force_login(self.a_test_user)
+        response = self.client.get(
+            reverse(
+                "detail",
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, USER_DETAIL_VIEW_TEMPLATE)
+
+    def test_current_user_is_context_object(self):
+        """
+        User should only be able to view their own account.
+        """
+        self.client.force_login(self.a_test_user)
+        response = self.client.get(
+            reverse(
+                "detail",
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["object"], self.a_test_user)
+
+    def test_view_returns_403_for_user_viewing_another_user(self):
+        """
+        User should not be able to view another user's account.
+        """
+        another_test_user = CustomUser.objects.create_user(
+            username=ANOTHER_TEST_USERNAME,
+            password=ANOTHER_TEST_PASSWORD,
+        )
+        self.client.force_login(another_test_user)
+        response = self.client.get(
+            reverse(
+                "detail",
+                kwargs={"pk": self.a_test_user.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 403)
