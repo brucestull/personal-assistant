21f923c30be9df6efe462d189511dcb2aac87221
Bruce Stull <bruce.stull@gmail.com>
2023-06-23 06:33:06 -0400

    Add link for 'home' to 'index' view.

--- valued_goals/views.py
+++ valued_goals/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the valued_goals index.")
+    return HttpResponse(
+        f"Hello, world. You're at the valued_goals index."
+        f"<br>"
+        f"<a href='/'>Home</a>"
+        )
     
