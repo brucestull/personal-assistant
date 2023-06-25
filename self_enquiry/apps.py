aa1bdb435f0551b19b977d183324595008a75534
Bruce Stull <bruce.stull@gmail.com>
2023-06-25 15:38:50 -0400

    In progress.

--- self_enquiry/apps.py
+++ self_enquiry/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SelfEnquiryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'self_enquiry'
+    verbose_name = "Self Enquiries"
