203692049bd1a1d5716bd6909325d3b19663a0e9
Bruce Stull <bruce.stull@gmail.com>
2023-08-02 19:22:17 -0400

    Testing without 'mixer'.

--- vitals/tests/test_admin.py
+++ vitals/tests/test_admin.py
@@ -1,20 +1,25 @@
 from django.contrib.auth.models import User
+from django.contrib import admin
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
-from mixer.backend.django import mixer
+
 from vitals.admin import VitalsAdmin, PulseAdmin
 from vitals.models import BloodPressure, Pulse
-from django.contrib import admin
-
+from accounts.models import CustomUser
 
 
 class VitalsAdminTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
+        self.user = CustomUser.objects.create_user(
             username="testuser",
             email="testuser@example.com",
             password="testpass",
+        )
+        self.blood_pressure = BloodPressure.objects.create(
+            user=self.user,
+            systolic=120,
+            diastolic=80,
         )
         self.admin = VitalsAdmin(BloodPressure, admin.site)
 
@@ -68,12 +73,22 @@ class VitalsAdminTest(TestCase):
 class PulseAdminTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
+        self.user = CustomUser.objects.create_user(
             username="testuser",
             email="testuser@example.com",
             password="testpass",
         )
+        self.pulse = Pulse.objects.create(
+            user=self.user,
+            bpm=60,
+        )
         self.admin = PulseAdmin(Pulse, admin.site)
+
+    def test_list_display(self):
+        self.assertEqual(
+            self.admin.list_display,
+            ("user", "bpm", "created"),
+        )
 
     def test_ordering(self):
         self.assertEqual(self.admin.ordering, ("-created",))
