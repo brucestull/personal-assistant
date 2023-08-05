29ac8412cfd311dd661e916f25e286cd922f02bf
Bruce Stull <bruce.stull@gmail.com>
2023-08-05 06:12:30 -0400

    Added 'app_tracker' 'verbose_name' of 'Application Tracker' to 'app_tracker/apps.py'.

--- app_tracker/apps.py
+++ app_tracker/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppTrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_tracker'
+    verbose_name = "Application Tracker"
