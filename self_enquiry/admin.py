83855f00fdcfe449740237de728ae041afa14a91
Bruce Stull <bruce.stull@gmail.com>
2023-08-24 19:03:53 -0400

    Removed 'created_add' read only requirement.

--- self_enquiry/admin.py
+++ self_enquiry/admin.py
@@ -31,8 +31,8 @@ class SelfEnquiryAdmin(admin.ModelAdmin):
     )
     # The `readonly_fields` attribute controls which fields are displayed as read-only on the update page for the model.
     readonly_fields = (
-        "created",
-        "updated",
+        # "updated",
+        # "created",
     )
     # The `fieldsets` attribute controls how the fields are grouped on the update page for the model.
     fieldsets = (
