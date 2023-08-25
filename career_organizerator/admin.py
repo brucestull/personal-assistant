f66c88d74acb67413e8c9df011e0678438c1a14c
Bruce Stull <bruce.stull@gmail.com>
2023-08-25 06:11:44 -0400

    Add 'BulletPoint' and 'ElevatorSpeech' models to 'admin.py'. Still need to create tests for these changes.

--- career_organizerator/admin.py
+++ career_organizerator/admin.py
@@ -1,3 +1,94 @@
 from django.contrib import admin
 
-# Register your models here.
+from career_organizerator.models import (
+    BulletPoint,
+    ElevatorSpeech,
+)
+
+
+@admin.register(BulletPoint)
+class BulletPointAdmin(admin.ModelAdmin):
+    """
+    Inherit from `admin.ModelAdmin` so we can customize the admin panel
+    for the `BulletPoint` model.
+    """
+
+    list_display = (
+        "text",
+        "created",
+        "user",
+    )
+    ordering = ("-created",)
+    list_filter = ("created",)
+    search_fields = (
+        "text",
+        "user__username",
+    )
+    readonly_fields = (
+        "created",
+        "updated",
+    )
+    fieldsets = (
+        (
+            None,
+            {"fields": (
+                "user",
+                "text",
+            )},
+        ),
+        (
+            "Dates",
+            {
+                "fields": (
+                    "created",
+                    "updated",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(ElevatorSpeech)
+class ElevatorSpeechAdmin(admin.ModelAdmin):
+    """
+    Inherit from `admin.ModelAdmin` so we can customize the admin panel
+    for the `ElevatorSpeech` model.
+    """
+
+    list_display = (
+        "theme",
+        "created",
+        "user",
+    )
+    ordering = ("-created",)
+    list_filter = ("created",)
+    search_fields = (
+        "theme",
+        "user__username",
+    )
+    readonly_fields = (
+        "created",
+        "updated",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "user",
+                    "theme",
+                    "text",
+                    "bullet_points",
+                )
+            },
+        ),
+        (
+            "Dates",
+            {
+                "fields": (
+                    "created",
+                    "updated",
+                )
+            },
+        ),
+    )
