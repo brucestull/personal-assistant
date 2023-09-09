8cccf3e032f4960e402d0d5a6134156d7eee7399
Bruce Stull <bruce.stull@gmail.com>
2023-09-09 09:14:02 -0400

    Removed 'unique=True' field option from 'Application.repository_url'.

--- app_tracker/models.py
+++ app_tracker/models.py
@@ -148,7 +148,6 @@ class Application(CreatedUpdatedBase):
         help_text="The URL of the application's repository.",
         null=True,
         blank=True,
-        unique=True,
     )
     reference_repository_url = models.URLField(
         verbose_name="Reference Repository URL",
