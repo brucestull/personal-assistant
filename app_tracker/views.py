ad20acc36f1c7d098bd10b8eb5350497356052b0
Bruce Stull <bruce.stull@gmail.com>
2023-09-09 09:08:23 -0400

    Edited docstring for 'home' view.

--- app_tracker/views.py
+++ app_tracker/views.py
@@ -13,6 +13,20 @@ from app_tracker.models import OrganizationalConcept
 def home(request):
     """
     View function for the home page of the `app_tracker` app.
+
+    This function adds the `the_site_name` and `page_title` variables to
+    the context dictionary, which are used in the base template to set
+    the page title and the site name.
+
+    Context variables:
+
+        the_site_name (str): The name of the site.
+
+            - This is set in the `THE_SITE_NAME` variable in `config/settings/common.py`.
+
+        page_title (str): The title of the page.
+
+            - This is set to "App Tracker Home" in this view function.
     """
     return render(
         request,
