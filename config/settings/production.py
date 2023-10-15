ec7ed3111a70be490a9f3d5e247b3a0f8aad1ca6
FlynntKnapp <bruce.stull@gmail.com>
2023-10-15 10:14:48 -0400

    Move 'ALLOWED_HOSTS' config settings to 'common.py'. Replaced single quotes with double quotes

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,18 +2,16 @@ import os
 
 from config.settings.common import *
 
-ALLOWED_HOSTS = ['flynnt-knapp-8e0b83ab9b88.herokuapp.com']
-
-MIDDLEWARE = MIDDLEWARE + ['whitenoise.middleware.WhiteNoiseMiddleware']
+MIDDLEWARE = MIDDLEWARE + ["whitenoise.middleware.WhiteNoiseMiddleware"]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # Create a specific `SECRET_KEY` for production and use it in production only.
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # To create a new `SECRET_KEY`:
 """
