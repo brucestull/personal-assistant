c619af58c281c14d283c906339dbfe7f84f1ff1d
FlynntKnapp <bruce.stull@gmail.com>
2023-10-15 10:45:55 -0400

    Move 'STATIC_ROOT' config setting to 'common.py'.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,7 +1,5 @@
 from config.settings.common import *  # noqa: F401
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
