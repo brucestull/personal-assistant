b082ad5d00f9fb4b6164651a51401ffc5f49b13e
FlynntKnapp <bruce.stull@gmail.com>
2023-10-26 18:27:40 -0400

    Refactor code to not use constants. This will make the actual value show in the test function for easiereadability.

--- app_tracker/tests/test_views.py
+++ app_tracker/tests/test_views.py
@@ -2,14 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-THE_SITE_NAME = "Personal Assistant"
-
-HOME_URL = "/app-tracker/"
-HOME_VIEW_NAME = "app_tracker:home"
-HOME_TEMPLATE = "app_tracker/home.html"
-HOME_PAGE_TITLE = "App Tracker Home"
-
-
 class HomeViewTest(TestCase):
     """
     Test the `home` view.
@@ -17,31 +9,36 @@ class HomeViewTest(TestCase):
 
     def test_home_view_url_exists_at_desired_location(self):
         """
-        Test that the `home` view is rendered at the desired location.
+        Test that the `home` view is rendered at "/app-tracker/".
         """
-        response = self.client.get(HOME_URL)
+        response = self.client.get("/app-tracker/")
         self.assertEqual(response.status_code, 200)
 
     def test_home_view_url_accessible_by_name(self):
         """
-        Test that the `home` view is rendered at the desired location by name.
+        Test that the `home` view is rendered at the desired location by
+        "app_tracker:home".
         """
-        response = self.client.get(reverse(HOME_VIEW_NAME))
+        response = self.client.get(reverse("app_tracker:home"))
         self.assertEqual(response.status_code, 200)
 
     def test_home_view_uses_correct_template(self):
         """
-        Test that the `home` view uses the correct template.
+        Test that the `home` view uses the correct template "app_tracker/home.html".
         """
-        response = self.client.get(reverse(HOME_VIEW_NAME))
+        response = self.client.get(reverse("app_tracker:home"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, HOME_TEMPLATE)
+        self.assertTemplateUsed(response, "app_tracker/home.html")
 
     def test_home_view_uses_correct_context(self):
         """
         Test that the `home` view uses the correct context.
+
+        The context should contain the following:
+        - the_site_name
+        - page_title
         """
-        response = self.client.get(reverse(HOME_VIEW_NAME))
+        response = self.client.get(reverse("app_tracker:home"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["the_site_name"], THE_SITE_NAME)
-        self.assertEqual(response.context["page_title"], HOME_PAGE_TITLE)
+        self.assertEqual(response.context["the_site_name"], "Personal Assistant")
+        self.assertEqual(response.context["page_title"], "App Tracker Home")
