96becfed036bffa9546ac4206b65f6aaf493704c
FlynntKnapp <bruce.stull@gmail.com>
2024-03-13 06:06:00 -0400

    Add 'tag' field to 'UnimportantNote' form.

--- unimportant_notes/forms.py
+++ unimportant_notes/forms.py
@@ -10,7 +10,7 @@ class UnimportantNoteForm(forms.ModelForm):
 
     class Meta:
         model = UnimportantNote
-        fields = ("title", "content", "url", "main_image")
+        fields = ("title", "tag", "content", "url", "main_image")
         # Is the `labals` attribute necessary?
         # labels = {
         #     "title": "Title",
