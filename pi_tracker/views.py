e5ea0785101a25db7fdcff7bd8963a7e735084db
Flynnt Knapp <bruce.stull@gmail.com>
2025-01-27 00:19:23 -0500

    Restrict PiDevice views to registration-accepted-authenticated users.

--- pi_tracker/views.py
+++ pi_tracker/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import CreateView, UpdateView
-from .models import PiDevice
+
+from base.mixins import RegistrationAcceptedMixin
+from base.decorators import registration_accepted_required
+
 from .forms import PiDeviceForm
+from .models import PiDevice
 
 
+@registration_accepted_required
 def pi_device_list(request):
     devices = PiDevice.objects.all()
     return render(request, 'pi_tracker/pi_device_list.html', {'devices': devices})
 
 
+@registration_accepted_required
 def pi_device_detail(request, pk):
     device = get_object_or_404(PiDevice, pk=pk)
     return render(request, 'pi_tracker/pi_device_detail.html', {'device': device})
 
 
-class PiDeviceCreateView(CreateView):
+class PiDeviceCreateView(RegistrationAcceptedMixin, CreateView):
     model = PiDevice
     form_class = PiDeviceForm
     template_name = 'pi_tracker/pi_device_form.html'
@@ -24,7 +30,7 @@ class PiDeviceCreateView(CreateView):
         return reverse('pi_device_list')
 
 
-class PiDeviceUpdateView(UpdateView):
+class PiDeviceUpdateView(RegistrationAcceptedMixin, UpdateView):
     model = PiDevice
     form_class = PiDeviceForm
     template_name = 'pi_tracker/pi_device_form.html'
