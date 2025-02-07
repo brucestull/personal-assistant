9fdee18c6d4e8f3f6562f5fd236fc3064d70d29a
FlynntKnapp <bruce.stull@gmail.com>
2025-02-07 06:57:27 -0500

    Add some links and views and such.

--- uc_goals/urls.py
+++ uc_goals/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = "uc_goals"
 urlpatterns = [
+    path("create/", views.GoalCreateView.as_view(), name="goal_create"),
+    path("<int:pk>/", views.GoalDetailView.as_view(), name="goal_detail"),
+    path("<int:pk>/update/", views.GoalUpdateView.as_view(), name="goal_update"),
     path("ucs/", views.ultimate_concerns, name="uc_list"),
     path("orphans/", views.orphan_goals, name="orphan_list"),
 ]
