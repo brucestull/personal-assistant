4380f9910d25a06b1d9d0a22c495a390731dd4fb
Flynnt Knapp <bruce.stull@gmail.com>
2025-02-11 05:09:36 -0500

    Field width adjustable.

---
+++ uc_goals/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+
+from .models import Goal
+
+
+class GoalForm(forms.ModelForm):
+    class Meta:
+        model = Goal
+        fields = [
+            "parent",
+            "name",
+            "is_ultimate_concern",
+            "description",
+            "due_date",
+            "completed",
+            "is_archived",
+        ]
+        labels = {
+            "parent": "Parent",
+            "name": "Name",
+            "is_ultimate_concern": "Is Ultimate Concern",
+            "description": "Description",
+            "due_date": "Due Date",
+            "completed": "Completed",
+            "is_archived": "Is Archived",
+        }
+        widgets = {
+            "parent": forms.Select(attrs={"class": "form-control"}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "is_ultimate_concern": forms.CheckboxInput(
+                attrs={"class": "form-check-input"}
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "style": "resize: both;",
+                }
+            ),
+            "due_date": forms.DateInput(attrs={"class": "form-control"}),
+            "completed": forms.CheckboxInput(attrs={"class": "form-check-input"}),
+            "is_archived": forms.CheckboxInput(attrs={"class": "form-check-input"}),
+        }
+        help_texts = {
+            "parent": "The parent goal of this goal.",
+            "name": "The name of this goal.",
+            "is_ultimate_concern": "Is this goal an ultimate concern?",
+            "description": "The description of this goal.",
+            "due_date": "The due date of this goal.",
+            "completed": "Is this goal completed?",
+            "is_archived": "Is this goal archived?",
+        }
+        error_messages = {
+            "name": {
+                "required": "The name of the goal is required.",
+            },
+        }
