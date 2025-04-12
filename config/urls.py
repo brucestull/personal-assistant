77e98a773e6568dcb9423bc3144ca18caa5dfa92
FlynntKnapp <bruce.stull@gmail.com>
2025-04-11 20:53:57 -0400

    Boosts app added. Let's check and see if this thing purrs.

--- config/urls.py
+++ config/urls.py
@@ -49,4 +49,5 @@ urlpatterns = [
     path("uc-goals/", include("uc_goals.urls")),
     path("journal/", include("journal.urls")),
     path("sonic-text/", include("sonic_text.urls")),
+    path("boosts/", include("boosts.urls")),
 ]
