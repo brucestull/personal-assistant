470a028df53d463df1330839f5fd8723a598dbbb
Flynnt Knapp <bruce.stull@gmail.com>
2025-05-30 14:48:53 -0400

    Update 'apps.py' to 'storages' from 'storage'.

--- storages/apps.py
+++ storages/apps.py
@@ -3,6 +3,6 @@
 from django.apps import AppConfig
 
 
-class StorageConfig(AppConfig):
+class StoragesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "storage"
