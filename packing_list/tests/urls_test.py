0a3dc87440292a3d8b6fde30f3d593c1f03580c5
Flynnt Knapp <bruce.stull@gmail.com>
2025-06-26 05:30:19 -0400

    Add testing for 'packing_list'.

---
+++ packing_list/tests/urls_test.py
@@ -0,0 +1,50 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from packing_list import views
+
+
+class UrlsTest(SimpleTestCase):
+    def test_activity_list_url(self):
+        url = reverse("packing_list:activity_list")
+        self.assertEqual(resolve(url).func, views.activity_list)
+
+    def test_activity_detail_url(self):
+        url = reverse("packing_list:activity_detail", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func, views.activity_detail)
+
+    def test_activity_create_url(self):
+        url = reverse("packing_list:activity_create")
+        self.assertEqual(resolve(url).func, views.activity_create)
+
+    def test_activity_update_url(self):
+        url = reverse("packing_list:activity_update", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func, views.activity_update)
+
+    def test_activity_delete_url(self):
+        url = reverse("packing_list:activity_delete", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func, views.activity_delete)
+
+    def test_activity_pdf_url(self):
+        url = reverse("packing_list:activity_pdf", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func, views.activity_pdf)
+
+    def test_item_list_url(self):
+        url = reverse("packing_list:item_list")
+        self.assertEqual(resolve(url).func, views.item_list)
+
+    def test_item_detail_url(self):
+        url = reverse("packing_list:item_detail", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func, views.item_detail)
+
+    def test_item_create_url(self):
+        url = reverse("packing_list:item_create")
+        self.assertEqual(resolve(url).func, views.item_create)
+
+    def test_item_update_url(self):
+        url = reverse("packing_list:item_update", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func, views.item_update)
+
+    def test_item_delete_url(self):
+        url = reverse("packing_list:item_delete", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func, views.item_delete)
