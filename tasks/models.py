6407f90a4733f6ac124f9ac847de31b30e306714
Flynnt Knapp <bruce.stull@gmail.com>
2025-07-14 10:29:37 -0400

    Add Tasks models.py

---
+++ tasks/models.py
@@ -0,0 +1,52 @@
+# tasks/models.py
+"""Models for the tasks app."""
+
+from django.db import models
+
+from base.models import CreatedUpdatedBase
+from config.settings import AUTH_USER_MODEL
+
+
+class Tag(CreatedUpdatedBase):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="tags"
+    )
+
+    def __str__(self):
+        return f"#{self.name}"
+
+
+class Priority(CreatedUpdatedBase):
+    name = models.CharField(max_length=50)
+    level = models.PositiveIntegerField(unique=True)
+    user = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="priorities"
+    )
+
+    class Meta:
+        ordering = ["level"]
+
+    def __str__(self):
+        return f"{self.name} (Level {self.level})"
+
+
+class Task(CreatedUpdatedBase):
+    name = models.CharField(max_length=100)
+    information = models.TextField(blank=True)
+    tag = models.ForeignKey(
+        Tag, on_delete=models.SET_NULL, null=True, related_name="tasks"
+    )
+    priority = models.ForeignKey(
+        Priority, on_delete=models.SET_NULL, null=True, related_name="tasks"
+    )
+    user = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="tasks"
+    )
+
+    class Meta:
+        ordering = ["priority__level", "created"]
+
+    def __str__(self):
+        return self.name
