378d0966349db8724559c27927fe4bc4bc2ac083
Flynnt Knapp <bruce.stull@gmail.com>
2025-08-07 20:13:26 -0400

    Remove 'pi_tracker' app.

--- config/urls.py
+++ config/urls.py
@@ -53,7 +53,6 @@ urlpatterns = [
     path("opportunity-search/", include("opportunity_search.urls")),
     path("project-manager/", include("project_manager.urls")),
     path("goals/", include("goals.urls")),
-    path("pi-tracker/", include("pi_tracker.urls")),
     path("uc-goals/", include("uc_goals.urls")),
     path("journal/", include("journal.urls")),
     path("sonic-text/", include("sonic_text.urls")),
