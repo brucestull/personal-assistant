c5724f9c51b9beccc41f710bdbdde08e217d4826
Flynnt Knapp <bruce.stull@gmail.com>
2025-08-07 20:00:33 -0400

    Remove 'storager' app.

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,6 @@ INSTALLED_APPS = [
     "boosts.apps.BoostsConfig",
     "plan_it.apps.PlanItConfig",
     "pomodo.apps.PomodoConfig",
-    "storager.apps.StoragerConfig",
     "story_line.apps.StoryLineConfig",
     "packing_list.apps.PackingListConfig",
     "decide.apps.DecideConfig",
--- storager/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StoragerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "storager"
