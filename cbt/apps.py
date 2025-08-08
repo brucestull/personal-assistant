a5ce7c8adc28033d016fa89ac98f0f4a31d6a827
Flynnt Knapp <bruce.stull@gmail.com>
2025-08-07 20:44:57 -0400

    Remove 'cbt' app.

--- cbt/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CbtConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cbt'
-    verbose_name = "Cognitive Behavioral Therapy"
