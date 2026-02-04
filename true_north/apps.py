287219592c4347582ae373edd58c3553513b91b7
Flynnt Knapp <bruce.stull@gmail.com>
2026-02-04 04:34:22 -0500

    Update 'true_north' app verbose name. Add demo seed data for 'true_north'.

--- true_north/apps.py
+++ true_north/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TrueNorthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "true_north"
+    verbose_name = "True North"
